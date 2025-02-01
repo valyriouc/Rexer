64381998775effeb25a28dba180810c292de0890
valacor <valyrious.informatics@gmail.com>
2025-02-01 13:15:29 +0100

    impl. back navigation

--- Backend/Commands/BackCommand.cs
+++ Backend/Commands/BackCommand.cs
@@ -2,13 +2,23 @@
 
 internal class BackCommand : IArgCommand
 {
-    public static IArgCommand FromArgs(string[] args)
-    {
-        throw new NotImplementedException();
-    }
+    private readonly HistoryStore _historyStore;
 
-    public Task ExecuteAsync(CancellationToken cancellationToken)
+    public BackCommand(HistoryStore historyStore)
     {
-        throw new NotImplementedException();
+        _historyStore = historyStore;
+    }
+    
+    public static IArgCommand FromArgs(string[] args) => 
+        new BackCommand(StoreHelper.History);
+
+    public async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        string current = Environment.CurrentDirectory;
+        _historyStore.Pop();
+        string last = _historyStore.Pop();
+        _historyStore.Push(current);
+        Console.WriteLine(last);
+        await Task.CompletedTask;
     }
 }
